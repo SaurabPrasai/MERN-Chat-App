ba001751c2449eb04fe789d90e810acb04afcfa4
Saurab Prasai <saurablovetocode@gmail.com>
2024-05-20 20:51:21 +0545

    complete getUsersForSidebar

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from "express"
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.route.js'
+import userRoutes from './routes/user.route.js'
 import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
 const app=express();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use('/api/messages',messageRoutes)
+app.use('/api/users',userRoutes)
+
 
 
 app.listen(PORT,()=>{
